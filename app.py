6d136ac33e56208d9eea58e067ee601b82137ebf
amjassem <adamjassem@gmail.com>
2023-12-25 18:27:21 +0100

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
-    return app
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
