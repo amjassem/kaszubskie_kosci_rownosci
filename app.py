e8ea23b8277b54aed905662ddce8553eac022e13
amjassem <adamjassem@gmail.com>
2023-12-25 19:02:03 +0100

    first commit

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import pandas as pd
-
+import os
 
 app = Flask(__name__)
-scores = pd.DataFrame(index=[0, 1], columns=["A", "B"])
+app.template_folder = os.path.abspath('templates')
 
+# Initialize a Pandas DataFrame to store values
+data = pd.DataFrame(columns=['User', 'Message'])
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "<h1>Test App</h1>"
+    # Display the stored values in the DataFrame
+    stored_values = data.to_html(index=False)
+    return render_template('index.html', stored_values=stored_values)
 
+@app.route('/send', methods=['POST'])
+def send():
+    user_message = request.form.get('message')
 
-if __name__ == "__main__":
-    app.run()
+    # Add the entered value to the DataFrame
+    data.loc[len(data)] = ['User', user_message]
+
+    # Display the stored values in the DataFrame
+    stored_values = data.to_html(index=False)
+    return render_template('index.html', stored_values=stored_values)
+
+if __name__ == '__main__':
+    app.run(debug=True)
